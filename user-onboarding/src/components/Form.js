39f48eb3061a0d6042dc5e4c11240ee0a4a22d4e
Robert <rc3331@gmail.com>
2019-09-19 15:35:19 -0500

    committing stretch goal to add styling used tailwindcss

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -11,21 +11,20 @@ const UserForm = ({ values, errors, touched, status }) =>{
         }
     }, [status])
     return (
-        <div className="user-form">
-            <Form>
-                <Field type="text" name="name" placeholder="name" />
-                {touched.name && errors.name && (<p>{errors.name}</p>)}
-                <Field type="text" name="email" placeholder="email" />
-                {touched.email && errors.email && (<p>{errors.email}</p>)}
-                <Field type="password" name="password" placeholder="password" />
-                {touched.password && errors.password && (<p>{errors.password}</p>)}
-                <label className="checkbox-container">
-                    {" "}
+        <div className="bg-blue-900 container half-width">
+            <Form className="flex-col justify-center items-center content-center p-8">
+                <h1 className="text-white text-6xl font-bold">Add User</h1>
+                <Field className="my-5 text-black bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-half appearance-none leading-normal" type="text" name="name" placeholder="name" />
+                {touched.name && errors.name && (<p className="text-white">{errors.name}</p>)}
+                <Field className="my-5 text-black bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-half appearance-none leading-normal" type="text" name="email" placeholder="email" />
+                {touched.email && errors.email && (<p className="text-white">{errors.email}</p>)}
+                <Field className="my-5 text black bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-half appearance-none leading-normal" type="password" name="password" placeholder="password" />
+                {touched.password && errors.password && (<p className="text-white">{errors.password}</p>)}
+                <input type="checkbox" name="tos" />
+                <label className="my-5 text-white checkbox-container">
                     Please select if you have read and agree to the terms of service.
-                    <Field component="checkbox" name="tos" checked={values.tos} />
-                    <span className="checkmark" />
-                </label>
-                <button type="submit">Submit</button> 
+                </label><br />
+                <button type="submit" className="my-5 bg-gray-500 hover:bg-gray-100 rounded py-4 px-10">Submit</button> 
             </Form>
             {users.map(user => (
                 <ul key={user.id}>
